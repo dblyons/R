d573b858b07877ebe90c87c6cbb52715f718f7cd
David Lyons <davidbrucelyons@gmail.com>
2022-01-05 18:36:50 +0000

    Update metaplot_from_ends_analysis_output.R

--- metaplot_from_ends_analysis_output.R
+++ metaplot_from_ends_analysis_output.R
@@ -17,32 +17,43 @@ filenames<-list.files("~/my_ends_output_matrices/",
                       pattern='.*Ends$', full.names = TRUE)
 
 
-h1 <- lapply(filenames, read.table)
+# populate a list with your matrices
+tmp_lis <- lapply(filenames, read.table)
 
-#make an empty list 
+#make an empty list for later 
 resultlis<-list()
 
 
 for (i in 1:length(filenames)) {  
   
-  # populate the list with previously matched of loci
-  # 
   
-  resultlis[[i]]<-simple.colmeans(h1[[i]])
+  
+  resultlis[[i]]<-simple.colmeans(tmp_lis[[i]])
   
   
 }
 
+# glue the lists together using cbind
 together<-do.call(cbind, resultlis)
+
+# convert to dataframe
 together<-as.data.frame(together)
+
+# add names to your new dataframe
 names(together)<-basename(filenames)
+
+# add the sequence position per window averaged -
+# for this setting, I run ends_analysis with b=2500 and d=50) 
 together<-data.frame(pos=seq(-2500,2450,50), together)
 melt.together<-melt(together, id='pos')
 
+# split the filename into multiple columns
+# so can subset as needed (for instance below I plot just generations I want, otherwise too noisy)
 melt_thing<-str_split_fixed(melt.together$variable, '\\.', 10)
 meltbig<-data.frame(melt.together, end=melt_thing[,7], 
                     generation=melt_thing[,2], genotype=melt_thing[,1], context=melt_thing[,5])
 
+# for color combinations, can make new ids like so 
 meltbig<-data.frame(meltbig, genotypeBygeneration=paste(meltbig$genotype, 
                                                         meltbig$generation))
 
@@ -59,7 +70,7 @@ ggplot(subset(meltbig, generation=="f2_1"|generation=="f4_1"|
   theme(axis.text=element_text(size=22))+ #
   theme(strip.text.x = element_blank())+#remove panel headers
   theme(strip.text.y = element_blank())+
-  scale_x_continuous(expand = c(0, 0), limits=c(-2000,2000))+#set limits of x
+  scale_x_continuous(expand = c(0, 0), limits=c(-2000,2000))+#set limits of x, important for continuous appearance
   ylim(0,0.75)+
   geom_vline(xintercept = 0,  col='grey10', lty=2)+
   labs(y='', x='')+#add axis lab
